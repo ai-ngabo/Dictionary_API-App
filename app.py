d580ef1ef060ca4ecba917a4eb8cc6a5cb07bb06
ai-ngabo <a.ngabo@alustudent.com>
2025-03-26 15:43:57 +0200

    changing codes of my flask scripts

--- app.py
+++ app.py
@@ -1,29 +1,11 @@
-from flask import Flask, request, jsonify
-import requests
+# This is a flask server to host my web app
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__) #Initializing the flask app
 
-# API configuration
-API_URL = "https://jobs-search-api.p.rapidapi.com/getjobs"
-HEADERS = {
-    "x-rapidapi-key": "b6b47bd245msh607ce92c7dc86b4p18cd62jsn8928279fedd2",
-    "x-rapidapi-host": "jobs-search-api.p.rapidapi.com",
-    "Content-Type": "application/json"
-}
+@app.route('/') # specifying the route(/) to access my Dictionary App
+def index():
+    return render_template('index.html') # Connecting Flask to my Html file
 
-@app.route("/api/jobs", methods=["POST"])
-def get_jobs():
-    # Get payload data from the frontend
-    payload = request.json
-
-    try:
-        # Send POST request to RapidAPI
-        response = requests.post(API_URL, json=payload, headers=HEADERS)
-        response.raise_for_status()
-        return jsonify(response.json())  # Return job data to frontend
-    except requests.exceptions.RequestException as e:
-        return jsonify({"error": str(e)}), 500
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
+if __name__ == '__main__':
+    app.run(debug=True) # Starting the server
