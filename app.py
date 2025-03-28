57be9575df5f360295d8a445aeeddc0bf2e91bae
ai-ngabo <a.ngabo@alustudent.com>
2025-03-28 13:58:43 +0200

    script renewal

--- app.py
+++ app.py
@@ -7,5 +7,7 @@ app = Flask(__name__) #Initializing the flask app
 def index():
     return render_template('index.html') # Connecting Flask to my Html file
 
+def dictionary():
+    return render_template('dictionary.html') # connecting to second page
 if __name__ == '__main__':
     app.run(debug=True) # Starting the server
